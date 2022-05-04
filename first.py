bf3ed2884a30cdea6b57e6d7bacb4cadd860be3b
Behrooz <90620602+behrooz-a@users.noreply.github.com>
2022-05-03 20:37:16 -0400

    Update first.py

--- first.py
+++ first.py
@@ -1,5 +1,8 @@
 number_1 = input("Enter the first number: ")
 number_2 = input("Enter the second number: ")
 result = int(number_1) * int(number_2)
+number_3=input("Enter the third number:")
+result1 = int(number_1) * int(number_2)
+result=result1/int(number_3)
 print("the result of multiplication: " + str(result) )
-print("the third one")
+
